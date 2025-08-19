fb111fc135e11ab0cf9cc18a8287a8f97bd547b3
Stefan Probst <stefanprobst@posteo.de>
2025-08-19 23:20:06 +0200

    build: bundle single output file

--- packages/content-lib/tsdown.config.ts
+++ packages/content-lib/tsdown.config.ts
@@ -3,11 +3,10 @@ import { defineConfig } from "tsdown";
 export default defineConfig({
 	clean: true,
 	dts: true,
-	entry: ["./src/*.ts", "!./src/*.test.ts"],
+	entry: "./src/index.ts",
 	format: ["esm"],
 	minify: false,
 	sourcemap: true,
 	treeshake: true,
-	unbundle: true,
 	tsconfig: "./tsconfig.build.json",
 });
