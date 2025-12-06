db499ea3a3bc8b1e10812e2641c6ec51e881b5f8
Stefan Probst <stefanprobst@posteo.de>
2025-12-06 18:18:50 +0100

    chore: update next (#43)

--- packages/content-lib/eslint.config.ts
+++ packages/content-lib/eslint.config.ts
@@ -1,9 +1,9 @@
 import baseConfig from "@acdh-oeaw/eslint-config";
 import nodeConfig from "@acdh-oeaw/eslint-config-node";
+import { defineConfig } from "eslint/config";
 import gitignore from "eslint-config-flat-gitignore";
-import { config } from "typescript-eslint";
 
-export default config(gitignore({ strict: false }), baseConfig, nodeConfig, {
+export default defineConfig(gitignore({ strict: false }), baseConfig, nodeConfig, {
 	rules: {
 		"arrow-body-style": ["error", "always"],
 		"@typescript-eslint/explicit-module-boundary-types": "error",
